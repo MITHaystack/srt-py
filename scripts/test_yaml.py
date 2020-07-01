9c057ba81c4cd7a06f2729d5f43ab5bcdbf4f242
Blaine Huey <blaineh@mit.edu>
2020-07-01 13:50:01 -0400

    Added a Brief YAML Test Script

---
+++ scripts/test_yaml.py
@@ -0,0 +1,10 @@
+"""test_yaml.py
+
+Validates and Prints the YAML Dictionary
+
+"""
+from utilities.yaml_tools import validate_yaml_schema, load_yaml
+
+if __name__ == "__main__":
+    validate_yaml_schema()
+    print(load_yaml())
