b20d5f5da8537f09dcafe0f9303c671a9fe65c28
Daniel Harrington <dmharrington03@gmail.com>
2023-02-21 11:24:38 -0500

    initial commit

---
+++ srt/dashboard/messaging/user_data_handler.py
@@ -0,0 +1,45 @@
+"""user_data_handler.py
+
+Class That Handles Reading and Writing User Data to a Database
+
+"""
+
+import sqlite3, werkzeug, os
+
+class UserDatabase():
+
+    def __init__(self):
+        pass
+    
+    def open_database():
+        pass
+
+    """ Account Handling Functions """
+    def register_user():
+        pass
+
+    def verify_user():
+        pass
+
+    def delete_user():
+        pass
+
+    def update_password_hash():
+        pass
+
+    def is_valid_login():
+        pass
+
+    """ Information getters (could also use @property decorator) """
+    def get_email():
+        pass
+
+    def get_n_scheduled_observations():
+        pass
+
+    def get_all():
+        pass
+
+    
+
+
