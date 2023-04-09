42e91c22b5e63623bf178dd2791e7013fe9b107e
Daniel Harrington <dmharrington03@gmail.com>
2023-04-09 00:27:35 -0400

    now creates observation table

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -4,6 +4,7 @@ Initializes the Users database table
 from sqlalchemy import Table, create_engine
 from flask_sqlalchemy import SQLAlchemy
 from srt.dashboard.messaging.user import User
+from srt.dashboard.messaging.observation import Observation
 
 
 # Connect to the database
@@ -12,9 +13,11 @@ conn = engine.connect()
 db = SQLAlchemy()
 
 Users_tbl = Table('user', User.metadata)
+Obs_tbl = Table('observation', Observation.metadata)
 
 def create_tables():
     User.metadata.create_all(engine)
+    Observation.metadata.create_all(engine)
 
 if __name__ == "__main__":
     create_tables()
