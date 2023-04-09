f2d7890d82a5bd30fe22e5b4e125b299e5cffd6c
Daniel Harrington <dmharrington03@gmail.com>
2023-04-08 23:15:20 -0400

    renamed class to follow conventions

--- srt/dashboard/messaging/user.py
+++ srt/dashboard/messaging/user.py
@@ -7,17 +7,11 @@ See https://flask-login.readthedocs.io/en/latest/
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-from sqlalchemy import Table, create_engine
-import sqlite3
 
 db = SQLAlchemy()
 
-class Users(db.Model, UserMixin):
-    """
-    Need:
-    insert into database
-    check if user exists
-    """
+class User(db.Model, UserMixin):
+    
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
@@ -26,34 +20,3 @@ class Users(db.Model, UserMixin):
     validated = db.Column(db.Boolean, default=False, nullable=False)
     admin = db.Column(db.Boolean, default=False, nullable=False)
     n_scheduled_observations = db.Column(db.Integer, default=0, nullable=False)
-
-    
-    # @property
-    # def is_authenticated(self) -> bool:
-    #     """ 
-    #     returns: True if user is authenticated, else False
-    #     """
-    #     return self.authenticated
-
-    # @property
-    # def is_active(self) -> bool:
-    #     """ 
-    #     returns: True if user has validated email, else False
-    #     """
-    #     # TODO
-    #     # return self.validated
-    #     return True
-    
-    # @property
-    # def is_anonymous(self) -> bool:
-    #     """ 
-    #     returns: True if user is anonymous, else False
-    #     """
-    #     # TODO
-    #     return False
-
-    # def get_id(self) -> str:
-    #     """ 
-    #     returns: str containing the user's ID
-    #     """
-    #     return str(self.id)
