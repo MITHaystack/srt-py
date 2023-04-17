25763dd25757d24675c07d35e86ae4b76d50f76b
Daniel Harrington <dmharrington03@gmail.com>
2023-04-17 19:19:47 -0400

    commit_observation function, function contracts

--- srt/dashboard/layouts/observation_queue.py
+++ srt/dashboard/layouts/observation_queue.py
@@ -28,15 +28,35 @@ from ..messaging.observation import Observation
 
 from ...dashboard import db
 
+
+def commit_observation(config: dict, user: User) -> None:
+    """Create a new observation for a user and add it to the database
+
+    Parameters
+    ----------
+    config : dict - observations settings. See observation.py
+    user: Flask-login user proxy
+
+    Returns
+    -------
+    None
+    """
+
+    obs = Observation.from_dict(config)
+    obs.user = user
+    user.n_scheduled_observations += 1
+    db.session.commit()
+
+
 def generate_queue_list(user):
     
     if user:
 
         layout = html.Div([
+            html.H3(f"User {user.name} has {user.n_scheduled_observations} observations scheduled"),
             html.Ul([
-                html.H3(f"User {user.name} has {user.n_scheduled_observations} observations scheduled"),
-                html.Li("Observation 1: Some info"),
-                html.Li("Observation 2: Some info"),
+                html.Li(f"Observation: {obs.obs_name}")
+                for obs in user.observations
             ]),
             dcc.Input(placeholder="Observation Name",
                       type="text",
@@ -61,24 +81,38 @@ def register_obs_callbacks(app, user) -> None:
     -------
     None
         """
+    
     @app.callback(Output("obs-status", "children"),
                   Input("new-obs-button", "n_clicks"),
                   [State("obs-name", "value")])
     def schedule_and_update_output(n: int, name: str) -> html.Div:
-        if not name:
-            return [""]
-        if user.is_authenticated:
-            obs = Observation(obs_name=name,
-                            scheduled_time=datetime.now(),
-                            obs_start_time=datetime.now(),
-                            ra="05,31,30", dec="05,31,30",
-                            duration=600,
-                            output_file_name="hello.fits",
-                            user=user)
-            user.n_scheduled_observations += 1
-            db.session.commit()
-            return html.Div(html.H4(f"Observation {name} successfully scheduled for user {user.name}, now has {user.n_scheduled_observations} scheduled"))
+        """Registers the Callbacks for the Observation Queue
 
+        Parameters
+        ----------
+        n : int - number of clicks on button
+        name: str - name of the observation
+        Returns
+        -------
+        None
+        """
+        if n > 0:
+            if not name:
+                return [""]
+            if user.is_authenticated:
+                obs = Observation(obs_name=name,
+                                scheduled_time=datetime.now(),
+                                obs_start_time=datetime.now(),
+                                ra="05,31,30", dec="05,31,30",
+                                duration=600,
+                                output_file_name="hello.fits",
+                                user=user)
+                user.n_scheduled_observations += 1
+                db.session.commit()
+                return html.Div(html.H4(f"Observation {name} successfully scheduled for user {user.name}, now has {user.n_scheduled_observations} scheduled"))
+        else:
+            raise PreventUpdate()
+    
         
 
 
